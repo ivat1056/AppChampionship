a55e43317522fb6c707a5793a555b3757aad78dd
ivanmatveev <ivat1056@gmail.com>
2023-03-06 07:23:44 +0300

    12

---
+++ app/src/main/java/com/example/appchampionship/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.appchampionship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
+    }
+
+}
